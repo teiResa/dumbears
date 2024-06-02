cef3acdcedc31f2f5ff9667b7a3ca053fecad01c
Theresa Balsdon <resablsdn@outlook.com>
2024-06-03 02:25:34 +0800

    alerts players, modal

--- assets/script1.js
+++ assets/script1.js
@@ -1,3 +1,19 @@
+/*
+Author: Theresa Balsdon (@TeiResa on Github/Gitlab)
+Description: Scripts for my projected called "Dumbears", which is to help non-mandarine speakers know why they just woke up the screaming air-raid sirens. The main purpose of this site is to ease the anxieties and panic.
+
+TOC:
+
+line      Section
+15        Time Piece
+36        Modal: Audio Player
+
+*/
+
+
+
+/* ~~~~~~~~~~~~~ TIME PIECE ~~~~~~~~~~~~~ */
+
 
 let getChinaTimeEN = function(){
  document.getElementById('date_time_EN').innerHTML = new Date().toLocaleString('en-CA', {timeZone: 'Asia/Shanghai', timeStyle: 'short', hourCycle:'h12', dateStyle: 'medium'});
@@ -13,3 +29,34 @@ let getChinaTimeZH = function(){
 getChinaTimeZH();
 setInterval(getChinaTimeZH, 1000);
 
+
+
+
+
+/* ~~~~~~~~~~~~~~~~ MODAL ~~~~~~~~~~~~~~~ */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
