09a2cd546701cdd8d20d2e70531b2465afb765dd
Theresa Balsdon <resablsdn@outlook.com>
2024-06-04 23:19:06 +0800

    see modal

--- assets/script1.js
+++ assets/script1.js
@@ -33,37 +33,83 @@ setInterval(getChinaTimeZH, 1000);
 
 
 
-/* ~~~~~~~~~~~~~~~~ MODAL ~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~ LISTEN MODAL ~~~~~~~~~~~~~ */
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modalListen = document.getElementById("myModal");
+var modalLook = document.getElementById("myLookModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btnLook = document.getElementById("seeBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var spanLook = document.getElementById("closeLook");
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modalListen.style.display = "block";
+}
+btnLook.onclick = function() {
+  modalLook.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modalListen.style.display = "none";
 }
+
+spanLook.onclick = function() {
+  modalLook.style.display = "none";
+}
+
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalListen | event.target == modalLook) {
+    modalListen.style.display = "none";modalLook.style.display = "none";
   }
 }
+
 
 /* idk if this works for audio player...
 
 
 let notTooLoud = document.getElementsByClassName("the_audio_playas");
 notTooLoud.volume = 0.5;
+*/
+
+
+
+
+/* ~~~~~~~~~~~~ LOOK MODAL ~~~~~~~~~~~ 
+
+// Get the modal
+var modalLook = document.getElementById("myLookModal");
+
+// Get the button that opens the modal
+var btnLook = document.getElementById("seeBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btnLook.onclick = function() {
+  modalLook.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalLook.style.display = "none";
+  modalListen.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalLook) {
+    modalLook.style.display = "none";
+  }
+}
 */
